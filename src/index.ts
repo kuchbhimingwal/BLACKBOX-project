1e5ef058567606c49ba32713a8139a8a1d11ae60
kuchbhimingwal <shubhammingi@gmail.com>
2024-05-19 10:43:59 -0400

    added signup and login rout

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { Hono } from 'hono'
 import userRoute from './routes/users'
 import postServer from './routes/posts'
-const app = new Hono<{
-  Bindings: {
-		DATABASE_URL: string,
-    JWT_SECRET: string
-	},}>()
+const app = new Hono()
 
 app.route('/user', userRoute)
 app.route('/post', postServer)
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,70 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
-const prisma = new PrismaClient()
-const userRoute = new Hono()
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+const userRoute = new Hono<{
+  Bindings: {
+		DATABASE_URL: string,
+    JWT_SECRET: string
+	}}>()
 
 userRoute.get('/signup', async(c) => {
-  const body = await c.req.parseBody()
-  return c.json(body)
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  const secret = c.env.JWT_SECRET;
+  try {
+    const res = await prisma.user.create({
+      data:{
+        name: body.name,
+        email: body.email,
+        password: body.password
+      }
+    })
+    const payload = {
+      userId: res.id,
+      name: res.name,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60, // Token expires in 1 hour
+    }
+    const token = await sign(payload, secret)
+    
+    return c.json({token: token, message: "Siggen up"})
+  } catch (error) {
+    console.log(error);
+    return c.json({message: "error while logging in!!"})
+  }
+})
+
+userRoute.get('/login', async(c)=>{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  const secret = c.env.JWT_SECRET;
+  try {
+    const res = await prisma.user.findUnique({
+      where:{
+        email: body.email,
+        password: body.password
+      }
+    });
+    if(!res){
+      return c.json({message:"Isse while logging in"})
+    }
+    const payload = {
+      userId: res.id,
+      name: res.name,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60, // Token expires in 1 hour
+    }
+    const token = await sign(payload, secret)
+    return c.json({message: "logged in", token: token})
+  } catch (error) {
+    console.log(error);
+    return c.json({message: "Cant log in"})
+  }
 })
 
 export default userRoute
