6b29119e83ba61b42243ee62371208c7ab97ba5b
kuchbhimingwal <shubhammingi@gmail.com>
2024-05-23 12:07:43 -0400

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono'
 import userRoute from './routes/users'
 import postServer from './routes/posts'
+import { cors } from  'hono/cors'
 const app = new Hono()
 
+app.use('/*', cors())
 app.route('/user', userRoute)
 app.route('/post', postServer)
 
